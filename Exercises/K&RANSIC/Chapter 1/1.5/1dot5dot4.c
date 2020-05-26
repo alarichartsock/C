835770711adb56c8e9b730b340141f61361ef8df
root <5252797+alarichartsock@users.noreply.github.com>
2020-05-26 11:09:16 -0700

    5252020 work

---
+++ Exercises/K&RANSIC/Chapter 1/1.5/1dot5dot4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define IN 1 //inside a word
+#define OUT 0 //outside a word
+
+//count lines, words, and characters in an input
+int main() {
+	int c, n1, nw, nc, state;
+	
+	state = OUT;
+	n1 = nw = nc = 0;
+	
+	while((c = getchar()) != EOF) {
+			++nc;
+			if (c == '\n')
+				++n1;
+			if (c == ' ' || c == '\n' || c == '\t')
+				state = OUT;
+			else if (state == OUT) {
+				state = IN;
+				++nw;
+			}
+	}
+	printf("%d %d %d\n",n1,nw,nc);
+	return 0;
+	}
+	
+//Exercise 1-11: You could manually count inputs and check for equivalency
+//Exercise 1-12: 
