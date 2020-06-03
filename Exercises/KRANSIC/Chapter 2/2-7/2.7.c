b2a90a9da8b0482fe3a38b0bfca8ddc42d270d5d
alarichartsock <5252797+alarichartsock@users.noreply.github.com>
2020-06-02 21:36:07 -0700

    622020 work
    Not much.

---
+++ Exercises/KRANSIC/Chapter 2/2-7/2.7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+	return 0;
+}
+
+unsigned invert(unsigned x, int p, int n) {
+
+	x 
+
+	return x;
+}
---
+++ Exercises/KRANSIC/Chapter 2/2-8/2.8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+	int i = 7;
+	int j = 7;
+
+	int k = ~i;
+	
+	printf("%i",k);
+
+	return 0;
+}
