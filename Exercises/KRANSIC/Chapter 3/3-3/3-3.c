ad8bc18637500bcb997df5877559a74934577fb1
alarichartsock <5252797+alarichartsock@users.noreply.github.com>
2020-06-05 22:24:16 -0700

    6552020 work

--- Exercises/KRANSIC/Chapter 3/3-3/3-3.c
+++ Exercises/KRANSIC/Chapter 3/3-3/3-3.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int main() {
-	int n = 1;
+void expand(char s1[], char s2[]);
 
+int main() {
+
+	char s1[6] = "12345";
+
+	printf("%c",s1[2]);
 
 	return 0;
+}
+
+void expand(char s1[], char s2[]) {
+	int i=0;
+	while(s1[i] != '\0') {
+		
+
+		i++;
+	}	
 }
